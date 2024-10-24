58bf017a2813fec32a19bf2f7eaba83b2022dd19
SebastianCardona-P <sebastian.cardona-p@mail.escuelaing.edu.co>
2024-10-24 14:23:04 -0500

    Add password encrytp and decrytp

---
+++ src/main/java/edu/eci/cvds/pattens/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package edu.eci.cvds.pattens.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf-> csrf.disable() )
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/login/**", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(); // Enable Basic Authentication for API
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
